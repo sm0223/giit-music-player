9c1fe96f2e4a7a10c8044920c8f566306e520281
hardeep0444 <45468267+hardeep0444@users.noreply.github.com>
2023-05-16 23:28:02 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -31,7 +31,7 @@ const newUserData = async (decodeValue, req, res) => {
     imageUrl:decodeValue.picture,
     user_id:decodeValue.user_id,
     email_verified:decodeValue.email_verified,
-    role:"ADMIN",
+    role:"MEMBER",
     auth_time:decodeValue.auth_time
   })
 
