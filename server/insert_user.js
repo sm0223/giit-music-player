a15359a17c8f11093ce11cd0ad8274424501a6ed
hardeep0444 <45468267+hardeep0444@users.noreply.github.com>
2023-05-16 23:54:26 +0530

    Update insert_user.js

--- server/insert_user.js
+++ server/insert_user.js
@@ -1,5 +1,9 @@
 db.createUser({
-  user: "myuser",
-  pwd: "mypassword",
-  roles: [{ role: "readWrite", db: "mydatabase" }]
+  "name": "Hardeep Singh Arora",
+  "email": "hardeep8011@gmail.com",
+  "imageUrl": "https://lh3.googleusercontent.com/a/AGNmyxaPy0wCsO6vbzAy_NUI172eMMwlUJg5lvTRCtNLBUg=s96-c",
+  "user_id": "2WEKjpsvXMTNITRogX26KcjMpz83",
+  "email_verified": true,
+  "auth_time": "1684166462",
+  "role": "ADMIN"
 });
